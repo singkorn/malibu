28ec7324dd494adca3466869282069e6f9da415c
nandap4790 <nandap4790@gmail.com>
2021-01-27 08:10:52 +0530

    min score change and circleci config change

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,21 +1,27 @@
 const url = `https://${process.env.LH_USER}:${process.env.LH_PASSWORD}@lighthouse-ci.staging.quinpress.com/`;
-const collectUrls = [
-  "https://malibu-qa-web.qtstage.io",
-  "https://malibu-perf.quintype.io/?uptime",
-  "https://malibu-web.qtstage.io",
-  "https://malibu-web.quintype.io"
-];
+// const collectUrls = [
+//   "https://malibu-qa-web.qtstage.io",
+//   "https://malibu-perf.quintype.io/?uptime",
+//   "https://malibu-web.qtstage.io",
+//   "https://malibu-web.quintype.io"
+// ];
+let minScore = 0.9;
+if (process.env.LHCI_SITES[0].includes("perf")) {
+  minScore = 0.75;
+} else if (process.env.LHCI_SITES[0].includes("staging")) {
+  minScore = 0.85;
+}
 const lhciConfig = {
   ci: {
     collect: {
-      url: collectUrls,
+      url: JSON.parse(process.env.LHCI_SITES),
       settings: {
         emulatedFormFactor: "mobile"
       }
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.9 }]
+        "categories:performance": ["error", { minScore: minScore }]
       }
     },
     upload: {
